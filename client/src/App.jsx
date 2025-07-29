8afcb1240af86e2a3a7005ae3924c6c1942d87f1
Mayank Kumar Sharma <mayankfhacker@gmail.com>
2025-07-30 02:27:27 +0530

    Frontend added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home.jsx'
 import { NotFound } from './pages/Notfound.jsx'
+import { Toaster } from "@/components/ui/toaster";
 
 export const App = () => {
   return (
    <>
+   <Toaster />
    <BrowserRouter>
       <Routes>
          <Route path="/" element={<Home />} />
