0076f1995f16d39b9f3ad12f926d761ba022090e
Mayank Kumar Sharma <mayankfhacker@gmail.com>
2025-08-01 17:37:47 +0530

    Backend Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home.jsx'
-import { NotFound } from './pages/Notfound.jsx'
+import { NotFound } from './pages/NotFound.jsx'
 import { Toaster } from "@/components/ui/toaster";
 
 export const App = () => {
