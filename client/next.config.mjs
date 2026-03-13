91ff7e043f16e9b96b53d337d69625610f05cc50
Mayank Kumar Sharma <mayankfhacker@gmail.com>
2026-03-13 19:20:46 +0530

    Refactor code structure for improved readability and maintainability

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
